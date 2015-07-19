e9ca7feac0582ca957d99e23e4ee4aecddb4501c
Anand Singh <anand@knoldus.com>
2015-07-19 16:50:39 +0530

    Removed unwanted code

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -18,48 +18,18 @@ import views._
 import java.util.concurrent.TimeoutException
 import play.api.libs.concurrent.Promise
 
-
 class Application extends Controller {
-  
+
+  implicit val timeout = 10.seconds
   implicit val empJsonFormat = Json.format[Employee]
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
-  
+
   def dashboard = Action {
     Ok(views.html.dashboard("Your new application is ready."))
   }
-  
-  // The json keys. The 'id' field was added as without it we would get a warning like this:
-  // Warning: Each child in an array or iterator should have a unique "key" prop. Check the render method of CommentList. See https://fb.me/react-warning-keys for more information.
-  val JSON_KEY_COMMENTS = "comments"
-  val JSON_KEY_AUTHOR = "author"
-  val JSON_KEY_TEXT = "text"
-  val JSON_KEY_ID = "id"
-
-  // Initialise the comments list
-  var commentsJson: JsArray = Json.arr(
-    Json.obj(JSON_KEY_ID -> UUID.randomUUID().toString, JSON_KEY_AUTHOR -> "Pete Hunt", JSON_KEY_TEXT -> "This is one comment"),
-    Json.obj(JSON_KEY_ID -> UUID.randomUUID().toString, JSON_KEY_AUTHOR -> "Jordan Walke", JSON_KEY_TEXT -> "This is *another* comment")
-  )
-
-  // Returns the comments list
-  def comments = Action {
-    Ok(commentsJson)
-  }
-
-  // Adds a new comment to the list and returns it
-  def comment(author: String, text: String) = Action {
-    val newComment = Json.obj(
-      JSON_KEY_ID -> UUID.randomUUID().toString,
-      JSON_KEY_AUTHOR -> author,
-      JSON_KEY_TEXT -> text)
-    commentsJson = commentsJson :+ newComment
-    Ok(newComment)
-  }
-  
-    implicit val timeout = 10.seconds
 
   /**
    * Describe the employee form (used in both edit and create screens).
@@ -98,7 +68,7 @@ class Application extends Controller {
     val futureEmp: Future[Option[models.Employee]] = TimeoutFuture(Employee.findById(id))
     futureEmp.map {
       case Some(employee) => Ok("")
-      case None => NotFound
+      case None           => NotFound
     }.recover {
       case t: TimeoutException =>
         Logger.error("Problem found in employee edit process")
